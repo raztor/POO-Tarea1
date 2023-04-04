e7789ee72760a0097e3218b24d5d29df4d974c22
Raztor <contacto@webizy.cl>
2023-04-04 17:59:04 -0400

    Finalizado código Clase Door y clase Window
    Co-authored-by: andreavalfonzo <andreavalfonzo13@gmail.com>

--- src/Door.java
+++ src/Door.java
@@ -1,22 +1,24 @@
 public class Door {
     public Door () {
         magneticSensor = new MagneticSensor();
-        ...
+        state = State.CLOSE;
     }
     {
         id = nextId++;
     }
     public void open() {
-        ...
+        state = State.OPEN;
+        magneticSensor.moveMagnetAwayFromSwitch();
     }
     public void close() {
-        ...
+        state = State.CLOSE;
+        magneticSensor.putMagnetNearSwitch();
     }
     public String getHeader(){
         return "d"+id;
     }
     public int getState(){
-        ...
+        return state.ordinal();
     }
 
     private MagneticSensor magneticSensor;
