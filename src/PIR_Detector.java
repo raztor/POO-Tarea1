91404207e94401e8fe6241bfe0692244c24afd1a
fernando.dezv <fernando.dezv@gmail.com>
2023-04-16 13:37:57 -0400

    Creación Class Person y PIR_Detector.

---
+++ src/PIR_Detector.java
@@ -0,0 +1,14 @@
+public class PIR_Detector{
+
+    public PIR_Detector (){ //constructor, valores iniciales
+
+    }
+
+    public getState(){
+        //return state; (???)
+    }
+
+    private Sensor sensor;
+    private Person person;
+
+}
---
+++ src/Person.java
@@ -0,0 +1,9 @@
+public class Person{
+
+    public Person(){ //constructor
+        this.coord_x = 0.0;
+        this.coord_y = 0.0; //inicializado en el origen
+    }
+    protected float coord_x;
+    protected float coord_y;
+}
